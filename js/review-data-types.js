6ebcf6124c730091846f4ef2a3eebd5ed1e23b01
Dominique Dixon <domdrop92@hotmail.com>
2019-04-29 17:40:36 -0700

    review of data types

---
+++ js/review-data-types.js
@@ -0,0 +1,48 @@
+// integers //
+var a = 12;
+var b = 24;
+var c = 3;
+
+var diff = b - a;
+console.log(diff);
+
+var multi = a * b;
+console.log(multi);
+
+// strings //
+var str1 = 'hello'
+var str2 = 'its me'
+console.log(str1);
+console.log(str2);
+
+console.log(str1 + ' ' + str2);
+
+
+//boolean//
+var a = true;
+var b = false;
+
+console.log(25==52);
+console.log(56===54);
+console.log(isNaN(2000));
+
+
+//array//
+var arr1 = [];
+var arr1 = ['hello' , 'from' , 'the' , 'outside'];
+var arr2 = [ 'I' , 'must' , 'have' , 'called' , '1000' , 'times'];
+console.log(arr1);
+console.log(arr2);
+
+
+//object//
+var car = {
+    make = 'honda',
+    model = 'civic',
+    year = '2010',
+}
+var classes = {
+    favorite = 'math',
+    worst = 'history',
+    best = 'english',
+}
