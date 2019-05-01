8b474a5d0eaa8a994df8f2aa8166fdb42bef19ac
Dominique Dixon <domdrop92@hotmail.com>
2019-04-30 18:48:18 -0700

    learning while, for in and of loops

--- js/loops.js
+++ js/loops.js
@@ -2,21 +2,37 @@
 
 var fruits = [ 'apple' , 'orange' , 'banana' , 'pear' , 'blueberry' ];
 console.log(fruits);
-console.log(fruits[0]);
-console.log(fruits[1]);
-console.log(fruits[2]);
-console.log(fruits[3]);
-console.log(fruits[4]);
+//console.log(fruits[0]);
+//console.log(fruits[1]);
+//console.log(fruits[2]);
+//console.log(fruits[3]);
+//console.log(fruits[4]);
 
 //for loops
-for (var i = 0; i < 4 ; i++ ) {
-    console.log(fruits[i]);
-}
+//for (var i = 0; i < 4 ; i++ ) {
+ //   console.log(fruits[i]);
+//}
 
 // do while
 var index =0;
-do {
-console.log(fruits[index]);
-index++;
+//do {
+//console.log(fruits[index]);
+//index++;
+//} while (index < fruits.length);
+
+// while
+while (index < fruits.length) {
+    console.log(fruits[index]);
+    index++;
 }
-while (index < fruits.length);
+
+//for in 
+fruits.foo = 'test';
+for (var i in fruits){
+    console.log(i);
+}
+
+//for of
+for (var i of fruits) {
+    console.log(i);
+}
